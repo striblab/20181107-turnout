c3717ae57fd94a4d4aaa1bb82c99b220120327f3
Frey Hargarten <harga012@umn.edu>
2018-12-17 16:12:41 -0600

    updates

--- app/map.js
+++ app/map.js
@@ -134,8 +134,9 @@ class Map {
             })
             .style("stroke-width", '1')
             .style("opacity", function(d) {
-
-                if (d.properties.turnout_DENSITY >= 0.8) {
+                if (d.properties.turnout_DIFF == null) {
+                    return 0.4;
+                } else if (d.properties.turnout_DENSITY >= 0.8) {
                     return d.properties.turnout_DENSITY;
                 } else {
                     return d.properties.turnout_DENSITY - 0.1;
@@ -146,7 +147,7 @@ class Map {
                     return "#F2AF80";
                 } else if (d.properties.turnout_DIFF > 0) {
                     return self.colorScale(d.properties.turnout_DIFF);
-                } else if (d.properties.turnout_DIFF == 0) {
+                } else if (d.properties.turnout_DIFF == 0 || d.properties.turnout_DIFF == null) {
                     return "#dddddd";
                 }
             })
